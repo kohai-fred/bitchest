5dc9987c11d762ddc130d5af895e53d0607a775a
kohai-fred <frederick.vu.pro@gmail.com>
2023-08-18 13:42:12 +0200

    update: add presentation

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -6,22 +6,42 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class UsersSeeder extends Seeder
 {
     public function run(): void
     {
+        $faker = Faker::create();
+
         User::create([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('admin@example.comA1'),
+            'firstname' => $faker->firstName,
+            'lastname' => $faker->lastName,
+            'email' => 'admin1@example.com',
+            'password' => Hash::make('admin1@example.comA1'),
             'role' => 'admin',
+            'presentation' => $faker->realTextBetween(160, 200)
         ]);
         User::create([
-            'name' => 'Client',
-            'email' => 'client@example.com',
-            'password' => Hash::make('client@example.comA1'),
-            'role' => 'client',
+            'firstname' => $faker->firstName,
+            'lastname' => $faker->lastName,
+            'email' => 'admin2@example.com',
+            'password' => Hash::make('admin2@example.comA1'),
+            'role' => 'admin',
+            'presentation' => $faker->realTextBetween(160, 200)
         ]);
+        for ($i = 0; $i < 10; $i++) {
+            $email = "client$i@example.com";
+            $password = Hash::make($email . 'A1');
+
+            User::create([
+                'firstname' => $faker->firstName,
+                'lastname' => $faker->lastName,
+                'email' => $email,
+                'password' => $password,
+                'role' => 'client',
+                'presentation' => $faker->realTextBetween(160, 200)
+            ]);
+        }
     }
 }
