40a459ed3e95e700ff6384051f4a81cdbb9ff042
kohai-fred <frederick.vu.pro@gmail.com>
2023-09-01 16:26:27 +0200

    feat: start display data

--- src/pages/CryptoPage.tsx
+++ src/pages/CryptoPage.tsx
@@ -1,11 +1,132 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import { Mode, DeleteForever } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { getLatestCryptosCotation } from "@src/services/cryptos";
+import { getUserCookies } from "@src/utils/cookiesUser";
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 
 const CryptoPage = () => {
+  const user = getUserCookies();
+  const {
+    data: cryptosCotation,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["latest-cotation"],
+    queryFn: getLatestCryptosCotation,
+    staleTime: 60_000,
+  });
+
   return (
-    <Typography variant="h1" textAlign="center" mt={8}>
-      Page en cours de création
-    </Typography>
+    <>
+      <Typography variant="h1" fontSize={34}>
+        Cours des cryptos
+      </Typography>
+      <Typography variant="body2" mt={5} mb={1}>
+        Le : {cryptosCotation && cryptosCotation[0].latest_cotation.timestamp}
+      </Typography>
+      <Box>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Crypto-monnaie</TableCell>
+                <TableCell align="right">Cours</TableCell>
+                <TableCell align="right">Détails</TableCell>
+                <TableCell align="right">Acheter</TableCell>
+                <TableCell align="right">Vendre</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {cryptosCotation &&
+                cryptosCotation.map((crypto) => {
+                  return (
+                    <TableRow
+                      key={crypto.id}
+                      sx={{
+                        "&:last-child td, &:last-child th": { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        <Box
+                          sx={{ display: "flex", alignItems: "center", gap: 2 }}
+                        >
+                          <img
+                            src={`/src/assets/cryptos/${crypto.symbol}.png`}
+                            style={{ width: "1rem" }}
+                          />
+                          {crypto.name}
+                        </Box>
+                      </TableCell>
+                      {/* <TableCell
+                        align="right"
+                        sx={{
+                          color:
+                            user.role === "admin"
+                              ? "customColors.bitchest.dark"
+                              : "",
+                        }}
+                      >
+                        {user.role}
+                      </TableCell> */}
+                      <TableCell align="right">
+                        {crypto.latest_cotation.price} €
+                      </TableCell>
+                      <TableCell
+                        align="right"
+                        sx={{
+                          ">a": {
+                            textDecoration: "none",
+                            color: "primary.main",
+                            "&:hover": {
+                              color: "customColors.bitchest.main",
+                            },
+                          },
+                        }}
+                      >
+                        <Link to={`/${user?.role}/details/${crypto.id}`}>
+                          {crypto.symbol}
+                        </Link>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Button variant="outlined">Vendre</Button>
+                        {/* <IconButton
+                          color="info"
+                          // onClick={() => openModalEditUser(user)}
+                        >
+                          <Mode color="info" />
+                        </IconButton> */}
+                      </TableCell>
+                      <TableCell align="right">
+                        <Button variant="outlined">Acheter</Button>
+                        {/* <IconButton
+                          color="error"
+                          onClick={() => openConfirmationDeleteModal(user)}
+                        >
+                          <DeleteForever color="error" />
+                        </IconButton> */}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
   );
 };
 
---
+++ src/services/cryptos.ts
@@ -0,0 +1,26 @@
+import { CotationType, Crypto, LatestCotation } from "@src/types/cryptos";
+import axiosInstance from "./axiosInstance";
+
+export async function getLatestCryptosCotation(): Promise<LatestCotation[]> {
+  const [res, status, message] = await axiosInstance({
+    url: `/latest-cotations`,
+  });
+  if (status) {
+    throw new Error(message);
+  }
+  return res.data;
+}
+
+export async function getCryptoDetails(id: string): Promise<{
+  cryptocurrency: Crypto;
+  cotations: CotationType[];
+}> {
+  const [res, status, message] = await axiosInstance({
+    url: `/crypto/${id}`,
+  });
+
+  if (status) {
+    throw new Error(message);
+  }
+  return res.data;
+}
