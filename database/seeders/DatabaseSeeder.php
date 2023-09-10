a1f718aa9edb1ccf9ff1faaadf8da02f1b44df7b
kohai-fred <frederick.vu.pro@gmail.com>
2023-09-11 00:59:33 +0200

    update: launch wallet and transaction seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersSeeder::class);
         $this->call(CryptoCurrencySeeder::class);
         $this->call(CryptoCotationSeeder::class);
+        $this->call(WalletSeeder::class);
+        $this->call(TransactionSeeder::class);
     }
 }
