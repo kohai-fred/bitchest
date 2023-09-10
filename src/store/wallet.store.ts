6cc6bcf89e5f2dbbc1492427e75af25e1c1d10f4
kohai-fred <frederick.vu.pro@gmail.com>
2023-09-10 23:50:28 +0200

    update: add new function and change a few properties

--- src/store/wallet.store.ts
+++ src/store/wallet.store.ts
@@ -1,61 +1,85 @@
-import { set } from "js-cookie";
+import { getTransactionsHistory } from "@src/services/walletAndTransactions";
+import type {
+  OwnedCryptoType,
+  TransactionClientHistory,
+} from "@src/types/transaction.type";
 import { create } from "zustand";
 
 type WalletState = {
   wallet: {
     balance: string;
-    idsCryptoTransaction: number[];
-    allClientTransactions: [];
+    ownedCrypto: OwnedCryptoType[];
+    transactionClientHistory: TransactionClientHistory;
   };
 };
 
-type InitWallet = {
-  balance: string;
-  idsCryptoTransaction: [];
-  allClientTransactions: [];
-};
-
 type ActionWalletType = {
-  initWallet: (wallet: WalletState) => void;
+  initWallet: (wallet: WalletState["wallet"]) => void;
   addBalance: (balance: string) => void;
   setNewBalance: (balance: string) => void;
-  addNewIdsCryptoTransaction: (id: number) => void;
-  removeIdsCryptoTransaction: (id: number) => void;
+  getOwnedCryptoById: (id: number) => OwnedCryptoType;
+  getIdOfOwnedCryptos: () => number[];
+  removeOwnedCryptoById: (id: number) => void;
+  setOfOwnedCryptos: (crypto: OwnedCryptoType[]) => void;
+  updateTransactionClientHistory: () => void;
 };
 
 const initWallet: WalletState = {
   wallet: {
     balance: "0",
-    idsCryptoTransaction: [],
-    allClientTransactions: [],
+    ownedCrypto: [],
+    transactionClientHistory: {},
   },
 };
 
-export const useWalletStore = create<WalletState & ActionWalletType>((set) => ({
-  ...initWallet,
-  initWallet: (wallet) => set((state) => ({ ...state, ...wallet })),
-  addBalance: (balance) =>
-    set((state) => ({
-      ...state,
-      wallet: { ...state.wallet, balance: state.wallet.balance + balance },
-    })),
-  setNewBalance: (balance) =>
-    set((state) => ({ ...state, wallet: { ...state.wallet, balance } })),
-  addNewIdsCryptoTransaction: (id) => {
-    set((state) => {
-      if (state.wallet.idsCryptoTransaction.includes(id)) return { ...state };
-      state.wallet.idsCryptoTransaction.push(id);
+export const useWalletStore = create<WalletState & ActionWalletType>(
+  (set, get) => ({
+    ...initWallet,
+    initWallet: (wallet) =>
+      set((state) => ({ ...state, wallet: { ...wallet } })),
+    addBalance: (balance) =>
+      set((state) => ({
+        ...state,
+        wallet: { ...state.wallet, balance: state.wallet.balance + balance },
+      })),
+    setNewBalance: (balance) =>
+      set((state) => ({ ...state, wallet: { ...state.wallet, balance } })),
 
-      return { ...state };
-    });
-  },
-  removeIdsCryptoTransaction: (id) => {
-    set((state) => {
-      if (!state.wallet.idsCryptoTransaction.includes(id)) return { ...state };
-      const { idsCryptoTransaction } = state.wallet;
-      const index = idsCryptoTransaction.indexOf(id);
-      idsCryptoTransaction.splice(index, 1);
-      return { ...state };
-    });
-  },
-}));
+    getOwnedCryptoById: (id) => {
+      const { ownedCrypto } = get().wallet;
+      const crypto = ownedCrypto.filter((c) => c.id === id)[0];
+      return crypto;
+    },
+    getIdOfOwnedCryptos: () => {
+      const { ownedCrypto } = get().wallet;
+      const arrayOfIds = ownedCrypto.map((crypto) => crypto.id);
+      return arrayOfIds;
+    },
+    removeOwnedCryptoById: (id) => {
+      set((state) => {
+        const { ownedCrypto } = state.wallet;
+        const arrayOfIds = state.getIdOfOwnedCryptos();
+        const index = arrayOfIds.indexOf(id);
+        ownedCrypto.splice(index, 1);
+        return { ...state };
+      });
+    },
+    setOfOwnedCryptos: (crypto) =>
+      set((state) => ({
+        ...state,
+        wallet: { ...state.wallet, ownedCrypto: crypto },
+      })),
+    updateTransactionClientHistory: async () => {
+      const transactionClientHistory = await getTransactionsHistory();
+      set((state) => {
+        return {
+          ...state,
+          wallet: {
+            ...state.wallet,
+            transactionClientHistory,
+          },
+        };
+      });
+    },
+  })
+);
