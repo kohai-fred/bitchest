c884bdbcf4f43cda7a6779db466d00d087ebbc55
kohai-fred <frederick.vu.pro@gmail.com>
2023-09-11 00:44:35 +0200

    update: add balance for new client

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,10 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserFormRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Wallet;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -44,17 +46,23 @@ class UserController extends Controller
             'presentation' => $request->input('presentation'),
         ]);
 
+        if ($user->role === 'client') {
+            Wallet::create([
+                'user_id' => $user->id,
+                'balance' => 0,
+            ]);
+        }
+
         return response()->json(['message' => 'User created successfully', 'user' => $user]);
     }
 
 
     public function update(UserFormRequest $request, User $user)
     {
-        $validatedData = $request->validated();
-        $user->update($validatedData);
-        return response()->json(['message' => 'User updated successfully', 'user' => $user]);
-    }
+        $user->update($request->validated());
 
+        return response()->json(['message' => 'Profil mis à jour avec succès']);
+    }
     public function destroy(User $user)
     {
         $user->delete();
