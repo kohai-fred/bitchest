99daa443a0ad893b81e6fa38c5542f9b41195c0d
kohai-fred <frederick.vu.pro@gmail.com>
2023-09-11 00:36:27 +0200

    feat: get all crypto currencies

--- app/Http/Controllers/CryptoCurrencyController.php
+++ app/Http/Controllers/CryptoCurrencyController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CryptoCurrency;
 use Illuminate\Http\Request;
 
 class CryptoCurrencyController extends Controller
 {
-    //
+    public function index()
+    {
+        $currencies = CryptoCurrency::all();
+        return response()->json($currencies);
+    }
 }
